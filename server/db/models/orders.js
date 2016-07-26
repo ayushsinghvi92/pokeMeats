bdb61c4543b463e19316679ac0f446483d25788d
ishdaddy7 <seanhan1@gmail.com>
2016-07-26 14:37:22 -0400

    minor tweaks to the hooks in orders model

--- server/db/models/orders.js
+++ server/db/models/orders.js
@@ -24,13 +24,15 @@ module.exports = db.define('orders', {
         beforeCreate: function (order) {
             //don't allow a new order to be created as completed with either null address ids, or missing those fields
             if(order.checkout_status === 'complete' && (!order.shipping_address_id || !order.billing_address_id)){
-              throw new Error('You must supply a shipping or billing address id');
+              //throw new Error('You must supply a shipping or billing address id');
+              return sequelize.Promise.reject('You can\'t do that!');
             }
         },
         beforeUpdate: function(order) {
             //don't allow an order to be updated as completed with either null address ids, or missing those fields
             if(order.checkout_status === 'complete' && (!order.shipping_address_id || !order.billing_address_id)){
-              throw new Error('You must supply a shipping or billing address id');
+              //throw new Error('You must supply a shipping or billing address id');
+              return sequelize.Promise.reject('You can\'t do that!');
             }
             if(order.checkout_status === 'complete')
             order.order_date = sequelize.fn('NOW');
