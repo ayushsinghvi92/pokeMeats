4e3d5b51ed4d1dce309045ea768487ccc65e68b7
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2016-07-29 11:33:18 -0400

    add pokemon tiles

---
+++ browser/js/home/home.state.js
@@ -0,0 +1,12 @@
+app.config(function ($stateProvider) {
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'js/home/home.html',
+        controller: 'homeCtrl',
+        resolve: {
+        	allProducts: function(ProductFactory) {
+        		return ProductFactory.fetchAll();
+        	}
+        }
+    });
+});
