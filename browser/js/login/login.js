a03b189db8bc1f94376153dffa7cdf89c8644048
mosane <ayush.singhvi@nyu.edu>
2016-07-30 17:08:45 -0400

    fix login issue

--- browser/js/login/login.js
+++ browser/js/login/login.js
@@ -22,7 +22,5 @@ app.controller('LoginCtrl', function ($scope, AuthService, $state) {
         }).catch(function () {
             $scope.error = 'Invalid login credentials.';
         });
-
     };
-
 });
