170f8cb7c3ac59171d96f08276aef359eeb1062c
mosane <ayush.singhvi@nyu.edu>
2016-07-30 13:21:16 -0400

    fix login issue by removing resolve

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -1,10 +1,17 @@
-app.controller("homeCtrl", function($scope, allProducts, $stateParams){
-	$scope.pokemon = allProducts;
+app.controller("homeCtrl", function($scope, ProductFactory, $stateParams){
+	
+
+	ProductFactory.fetchAll()
+	.then(function (pokemon) {
+		$scope.pokemon = pokemon;
+	})
+
 	if ($stateParams.tag.length) {
 		$scope.pokemon = $scope.pokemon.filter(function(pokemon){
 			return pokemon.tags.includes($stateParams.tag)
 		})
 	}
+
 	$scope.toggleFilter = function (type) {
 		$scope.pokemon = allProducts.filter(function(pokemon){
 			return pokemon.type == type;
--- browser/js/home/home.state.js
+++ browser/js/home/home.state.js
@@ -2,11 +2,6 @@ app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/:tag',
         templateUrl: 'js/home/home.html',
-        controller: 'homeCtrl',
-        resolve: {
-        	allProducts: function(ProductFactory, $stateParams) {    
-                return ProductFactory.fetchAll();
-        	}
-        }
+        controller: 'homeCtrl'
     });
 });
