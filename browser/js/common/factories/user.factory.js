0e5541c92d49f4b420962186890db8bf4604b6bc
mosane <ayush.singhvi@nyu.edu>
2016-07-31 09:42:47 -0400

    try adding cart functionality to product page as well - still buggy however. DO NOT MERGE YET

--- browser/js/common/factories/user.factory.js
+++ browser/js/common/factories/user.factory.js
@@ -2,7 +2,7 @@ app.factory('userFactory', function (AuthService, $http, orderFactory) {
 
 	let getData = (res => res.data);
 	return {
-		getActiveOrder : function () {
+		getActiveOrderId : function () {
 			return AuthService.getLoggedInUser()
 			.then(user=>user.id)
 			.then(userId => $http.get('/api/users/' + userId + '/orders/')) 
@@ -12,7 +12,10 @@ app.factory('userFactory', function (AuthService, $http, orderFactory) {
 					return order.checkout_status === 'in_progress';
 				})
 			})
-			.then(orders => orders[0].id)
+			.then(orders => orders[0].id)			
+		},
+		getActiveOrder : function () {  
+			return this.getActiveOrderId()
 			.then(orderFactory.getAllOrderProducts)
 		}
 	}
