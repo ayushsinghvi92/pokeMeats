936c42a9133c97039414354b7201f65517084231
Jackson Brietzke <jack.brietzk@gmail.com>
2016-07-31 15:15:47 -0400

    adding to seedFile directly

--- dataWrangling/wrangle.js
+++ dataWrangling/wrangle.js
@@ -17,7 +17,7 @@ function pricing (x) {
 	return Number((100 / x).toFixed(2));
 }
 
-var items = ["Tail", "Feet", "Liver", "Tongue", "Belly", "Bacon", "Sausage", "Brain", "Shoulder", "Ears", "Toes"];
+var items = ["Tail", "Feet", "Liver", "Tongue", "Belly", "Bacon", "Sausage", "Brain", "Shoulder", "Ears"];
 var dummyParagraph = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 for (_pokemon in pokemon) {
 	formattedPokemon.push({
