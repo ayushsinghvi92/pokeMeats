a4a19250c68daa14312f67db4455bf9b2c4efdac
ishdaddy7 <seanhan1@gmail.com>
2016-08-01 12:56:58 -0400

    get myAccount past orders looking alright

--- browser/js/myAccount/myAccount.controller.js
+++ browser/js/myAccount/myAccount.controller.js
@@ -1,15 +1,16 @@
-app.controller('MyAccountController', function ($scope, UserFactory, AuthService, orderFactory) {
+app.controller('MyAccountController', function ($scope, userFactory, AuthService, orderFactory) {
     AuthService.getLoggedInUser().then(function (user) {
         $scope.user = user;
     });
 
-    UserFactory.fetchAllUserAddresses(5) //don't forget to change this
+    userFactory.fetchAllUserAddresses(5) //don't forget to change this
     .then(function(userAddresses){
       $scope.userAddresses = userAddresses;
     })
 
-    UserFactory.fetchAllUserOrders(5) // don't forget to change this - only orderId 1 has order Products, and orderId 1 belongs to userId 5
+    userFactory.fetchAllUserOrders(5) // don't forget to change this - only orderId 1 has order Products, and orderId 1 belongs to userId 5
     .then(function(userOrders){
+      console.log(userOrders)
       $scope.userOrders = userOrders;
       //this is almost working - need to figure out how to best get to Product details
     })
